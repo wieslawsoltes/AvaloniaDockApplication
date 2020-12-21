1e5c07ae0e928443a7eac5175bed18f99a3f1f3c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-12-21 20:42:41 +0100

    Use axaml

---
+++ AvaloniaDockApplication/App.axaml.cs
@@ -0,0 +1,68 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using AvaloniaDockApplication.Models;
+using AvaloniaDockApplication.ViewModels;
+using AvaloniaDockApplication.Views;
+using Dock.Model;
+
+namespace AvaloniaDockApplication
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            var factory = new MainDockFactory(new DemoData());
+            var layout = factory.CreateLayout();
+            factory.InitLayout(layout);
+
+            var mainWindowViewModel = new MainWindowViewModel()
+            {
+                Factory = factory,
+                Layout = layout
+            };
+
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+            {
+                
+                var mainWindow = new MainWindow
+                {
+                    DataContext = mainWindowViewModel
+                };
+
+                mainWindow.Closing += (sender, e) =>
+                {
+                    if (layout is IDock dock)
+                    {
+                        dock.Close();
+                    }
+                };
+
+                desktopLifetime.MainWindow = mainWindow;
+
+                desktopLifetime.Exit += (sennder, e) =>
+                {
+                    if (layout is IDock dock)
+                    {
+                        dock.Close();
+                    }
+                };
+            }
+            else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewLifetime)
+            {
+                var mainView = new MainView()
+                {
+                    DataContext = mainWindowViewModel
+                };
+
+                singleViewLifetime.MainView = mainView;
+            }
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
